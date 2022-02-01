960c6e408c3117c9d666cd65613f80dacee5b0e1
Craig Miller <craigmiller160@gmail.com>
2022-02-01 13:34:55 -0500

    Progress with locking experiment

--- src/main/kotlin/io/craigmiller160/pgtest/service/PersonService.kt
+++ src/main/kotlin/io/craigmiller160/pgtest/service/PersonService.kt
@@ -1,6 +1,11 @@
 package io.craigmiller160.pgtest.service
 
 import io.craigmiller160.pgtest.domain.PersonRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import javax.annotation.PostConstruct
@@ -9,11 +14,28 @@ import javax.annotation.PostConstruct
 class PersonService(
         private val personRepo: PersonRepo
 ) {
+
     @PostConstruct
     @Transactional
     fun stuff() {
-        val person = personRepo.findById(1L).get();
+        runBlocking {
+            launch {
+                doChange(2000)
+            }
+            launch {
+                doChange(4000)
+            }
+        }
+    }
+
+    private suspend fun doChange(time: Long) {
+        val person = withContext(Dispatchers.IO) {
+            personRepo.findById(1L).get()
+        }
+        delay(time)
         val newPerson = person.copy(accountBalance = person.accountBalance - 10)
-        personRepo.save(newPerson)
+        withContext(Dispatchers.IO) {
+            personRepo.save(newPerson)
+        }
     }
 }
