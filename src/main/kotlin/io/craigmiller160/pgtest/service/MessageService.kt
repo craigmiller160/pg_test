e7e303bc292dabe7f3c35b6ced844bfdcf5bd725
Craig Miller <craigmiller160@gmail.com>
2022-02-01 07:57:31 -0500

    Added logging

--- src/main/kotlin/io/craigmiller160/pgtest/service/MessageService.kt
+++ src/main/kotlin/io/craigmiller160/pgtest/service/MessageService.kt
@@ -1,12 +1,26 @@
 package io.craigmiller160.pgtest.service
 
 import io.craigmiller160.pgtest.domain.MessageRepo
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class MessageService(
         private val messageRepo: MessageRepo
 ) {
-    fun getMessage(): String =
-            messageRepo.findAll().first().message
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun getMessage(): String {
+        logger.info("Trying to find message in Postgres")
+        return runCatching {
+            val message = messageRepo.findAll().first().message
+            logger.info("Message found: $message")
+            message
+        }
+                .recover { ex ->
+                    logger.error("Error getting message from Postgres", ex)
+                    "Failed"
+                }
+                .getOrThrow()
+    }
 }
